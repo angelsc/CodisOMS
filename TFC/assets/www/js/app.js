ffee3be85270242cbbaea68a73ebb12bc48438d8
oriolikart <oriolikart@gmail.com>
2013-11-16 13:04:58 +0100

    Uri - 13.04 ( 1-11-2013 )

--- TFC/assets/www/js/app.js
+++ TFC/assets/www/js/app.js
@@ -5,9 +5,15 @@ var _app = new Object();
 /***
  * Funcion al hacer click sobre una imagen
  * */
-function searchImgDiangostics(attr){
-	$("#titleSearch").html("");			// reset
-	$("#titleSearch").html(attr);
+function searchImgDiangostics(title, subtitle , dtype){
+	$("#titleSearch").html("");				// reset Title
+	$("#titleSearch").html(title);
+	$("#subtitleSearch").html("");			// reset SubTitle
+	$("#subtitleSearch").html(subtitle);
+	
+	// Setter del tipo que se buscará
+	_app._type = dtype;
+
 	// Canvia de página
 	$.mobile.changePage( "#search", { transition: "slide", changeHash: true });
 }
@@ -15,9 +21,15 @@ function searchImgDiangostics(attr){
 /***
  * Funcion al hacer click sobre una imagen
  * */
-function searchImgProcediments(attr){
-	$("#titleSearch").html("");			// reset
-	$("#titleSearch").html(attr);
+function searchImgProcediments(title, subtitle , dtype){
+	
+	$("#titleSearch").html("");				// reset Title
+	$("#titleSearch").html(title);
+	$("#subtitleSearch").html("");			// reset SubTitle
+	$("#subtitleSearch").html(subtitle);
+	
+	// Setter del tipo que se buscará
+	_app._type = dtype;
 	// Canvia de página
 	$.mobile.changePage( "#search", { transition: "slide", changeHash: true });
 }
@@ -28,12 +40,45 @@ function searchImgProcediments(attr){
  * Busca dentro de los códigos V 
  **/
 function searchV(){
-	var attr = "";
-	$("#titleSearch").html(attr);
-	_app._type = "";
+	$("#titleSearch").html("");				// reset Title
+	$("#titleSearch").html("Codigos V");
+	$("#subtitleSearch").html("");			// reset SubTitle
+	$("#subtitleSearch").html("Buscar codigos V");
+	// Setter del tipo que se buscará
+	_app._type = "40";
 	// Canvia de página
 	$.mobile.changePage( "#search", { transition: "slide", changeHash: true });
 }
+
+/**
+ * Busca dentro de los códigos E
+ **/
+function searchE(){
+	$("#titleSearch").html("");				// reset Title
+	$("#titleSearch").html("Codigos E");
+	$("#subtitleSearch").html("");			// reset SubTitle
+	$("#subtitleSearch").html("Buscar codigos E");
+	// Setter del tipo que se buscará
+	_app._type = "50";
+	// Canvia de página
+	$.mobile.changePage( "#search", { transition: "slide", changeHash: true });
+}
+
+/**
+ * Busca dentro de los códigos M
+ **/
+function searchM(){
+	$("#titleSearch").html("");				// reset Title
+	$("#titleSearch").html("Codigos M");
+	$("#subtitleSearch").html("");			// reset SubTitle
+	$("#subtitleSearch").html("Buscar codigos M");
+	// Setter del tipo que se buscará
+	_app._type = "60";
+	// Canvia de página
+	$.mobile.changePage( "#search", { transition: "slide", changeHash: true });
+}
+
+
 
 /***
  * Pagina buscar codigos
@@ -51,11 +96,11 @@ function searchCodes(){
 $(function() {
 	 $(".search_img_diagnostics").click(function(){
 		 // Passem a la funció searchImg atributs "custom"
-		 searchImgDiangostics($(this).attr("dtype"));
+		 searchImgDiangostics( $(this).attr("titleSearch") , $(this).attr("subtitleSearch") ,  $(this).attr("dtype")  );
 	 });
 	 $(".search_img_procediments").click(function(){
 		 // Passem a la funció searchImg atributs "custom"
-		 searchImgProcediments($(this).attr("dtype"));
+		 searchImgProcediments( $(this).attr("titleSearch") , $(this).attr("subtitleSearch") ,  $(this).attr("dtype")  );
 	 });
 	 
 	 
